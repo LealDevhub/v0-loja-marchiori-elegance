06b7a65bf0b85e2c5cf4b5c69ccd5dcd54f54baa
lealdevhub <fabricio.pereira.leal@gmail.com>
2026-03-29 16:14:17 -0300

    Alterando foto do metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from 'next'
-import { Playfair_Display, Inter } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
 import { CartProvider } from '@/lib/cart-context'
+import { Analytics } from '@vercel/analytics/next'
+import type { Metadata } from 'next'
+import { Inter, Playfair_Display } from 'next/font/google'
 import './globals.css'
 
 const playfair = Playfair_Display({ 
@@ -21,17 +21,8 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
+        url: 'https://instagram.fcgh19-1.fna.fbcdn.net/v/t51.82787-19/561960331_17842352019599119_4075527416018453761_n.jpg?',
+      }
     ],
     apple: '/apple-icon.png',
   },
